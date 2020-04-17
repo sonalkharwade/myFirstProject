31fded0516a6b6b190e27a061ffa32a31cdf97d4
sonal <sonalkharwade21@gmail.com>
2020-04-17 15:12:03 +0530

    Commiting Changes

---
+++ App.java
@@ -0,0 +1,16 @@
+package myFirstProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "My first project" );
+		System.out.println( "Ready to commit" );
+
+    }
+}
